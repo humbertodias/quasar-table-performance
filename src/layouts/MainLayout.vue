f74f6283721862d7539758afd57373434265b414
Humberto Dias <hldtux@gmail.com>
2023-03-02 23:56:36 -0300

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="q-pa-md">
+    <q-table
+      grid
+      title="Treats"
+      :rows="rows"
+      :columns="columns"
+      row-key="name"
+      :filter="filter"
+      hide-header
+      :rows-per-page-options="[0]"
+    >
+      <template v-slot:top-right>
+        <q-input
+          borderless
+          dense
+          debounce="300"
+          v-model="filter"
+          placeholder="Search"
+        >
+          <template v-slot:append>
+            <q-icon name="search" />
+          </template>
+        </q-input>
+      </template>
+    </q-table>
+  </div>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+// import EssentialLink from "components/EssentialLink.vue";
+import { faker } from "@faker-js/faker";
+
+const columns = [
+  {
+    name: "desc",
+    required: true,
+    label: "Dessert (100g serving)",
+    align: "left",
+    field: (row) => row.name,
+    format: (val) => `${val}`,
+    sortable: true,
+  },
+  {
+    name: "calories",
+    align: "center",
+    label: "Calories",
+    field: "calories",
+    sortable: true,
+  },
+  { name: "fat", label: "Fat (g)", field: "fat", sortable: true },
+  { name: "carbs", label: "Carbs (g)", field: "carbs" },
+];
+
+const rows = [
+  {
+    name: "Frozen Yogurt",
+    calories: 159,
+    fat: 6.0,
+    carbs: 24,
+  },
+];
+
+export default defineComponent({
+  name: "MainLayout",
+
+  components: {},
+
+  setup() {
+    for (var n = 0; n < 2000; ++n) {
+      rows.push({
+        name: faker.name.fullName(),
+        calories: Math.floor(Math.random() * 10),
+        fat: Math.floor(Math.random() * 10),
+        carbs: Math.floor(Math.random() * 10),
+      });
+    }
+
+    return {
+      columns,
+      rows,
+    };
+  },
+});
+</script>
